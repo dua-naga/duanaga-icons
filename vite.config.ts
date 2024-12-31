4589784bb35c54f828dc876676f7418be7fcce43
Maulana Aprizqy Sumaryanto <aprizqy.dev@gmail.com>
2024-12-31 13:16:46 +0700

    fix fonts not loaded correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import * as path from 'path'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
@@ -14,11 +15,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@MaterialSymbols': resolve('./public/fonts/material-symbols-outlined.woff2')
     }
   },
   build: {
-    copyPublicDir: true,
+    copyPublicDir: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'DuanagaIcons',
